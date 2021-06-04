b94c805903fee0ad833faec95d1e574c45bad2df
Marcos Santos <marcosalmeida1977@gmail.com>
2021-06-03 21:03:11 -0700

    first commit

---
+++ src/main/java/com/santos/student/EurekaClientStudentServiceApplication.java
@@ -0,0 +1,15 @@
+package com.santos.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class EurekaClientStudentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientStudentServiceApplication.class, args);
+	}
+
+}
